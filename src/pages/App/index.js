ad6e5e59bc74a1c9c741ea3e7e53fab27efd9e05
Yalalt <yalaltgandush@gmail.com>
2023-01-04 23:13:30 +0800

    Folders files created

---
+++ src/pages/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+import Toolbar from '../../components/Toolbar';
+import BurgerBuilder from '../BurgerBuilder';
+
+function App() {
+  return (
+    <div>
+      <Toolbar />
+      <main className='Content'>
+      <BurgerBuilder />
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/BurgerBuilder/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class BurgerBuilder extends React.Component {
+  render() {
+    return (<div>
+      <div>Hachirtai bread</div>
+      <div>Ingredients</div>
+    </div>);
+  }
+}
+
+export default BurgerBuilder;
